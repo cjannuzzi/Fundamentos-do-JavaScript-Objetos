661ae9c09a61cb99d26a89e90e09fd6e99ef6f34
Caio Jannuzzi <eng.caiojannuzzi@outlook.com>
2022-03-05 15:00:21 -0300

    Resolvendo o primeiro desafio.

---
+++ print-infos.js
@@ -0,0 +1,10 @@
+const cliente = {
+    nome: "Andre",
+    idade: 36,
+    cpf: "1235678954",
+    email: "andre@gmail.com"
+};
+
+console.log(`Meu nome é ${cliente.nome} e tenho ${cliente.idade} anos.`);
+
+console.log(cliente.cpf.substring(0,3));
