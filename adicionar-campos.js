83e28ffb994107c31e91dcaa9742f075afdba477
Caio Jannuzzi <eng.caiojannuzzi@outlook.com>
2022-03-05 15:42:23 -0300

    Resolver o 3º desafio adicionando uma nova chave .fone ao objeto cliente.

---
+++ adicionar-campos.js
@@ -0,0 +1,15 @@
+const cliente = {
+    nome: "Andre",
+    idade: 36,
+    cpf: "1235678954",
+    email: "andre@gmail.com"
+};
+
+console.log(cliente);
+
+cliente.fone = "24993075555";
+
+console.log(cliente);
+
+cliente.fone=("84997756321");
+console.log(cliente);
