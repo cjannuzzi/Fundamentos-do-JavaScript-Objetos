6f83ce5be78e1ee95a387781a9672a039f0c7888
Caio Jannuzzi <eng.caiojannuzzi@outlook.com>
2022-03-05 20:03:09 -0300

    Resolver o FAÇA VOCê mesmo usando um heroi como base de dados.

---
+++ obj-peter.js
@@ -0,0 +1,17 @@
+const novoVingador = {
+    nome: "Peter Parker",
+    idade: 17, 
+    atuação: "Estudante",
+    alterEgo: "Homem-Aranha",
+    familia: {
+        tia: {
+           nome: "May Parker",
+           idade: 53,
+        },
+        namorada: {
+            nome: "Mary Jane Watson",
+            idade: 17,
+        }
+    }
+};
+console.log(novoVingador);
