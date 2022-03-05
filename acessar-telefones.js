42f244a85e147f9a295e06ae25c71c5769cdd34b
Caio Jannuzzi <eng.caiojannuzzi@outlook.com>
2022-03-05 20:22:42 -0300

    Resolvendo o 4º desafio.

---
+++ acessar-telefones.js
@@ -0,0 +1,9 @@
+const cliente = {
+    nome: "Andre",
+    idade: 36,
+    cpf: "1235678954",
+    email: "andre@gmail.com",
+    fones: ["559123598","552198873124"]
+};
+cliente.fones.forEach(fone => console.log(fone));
+
