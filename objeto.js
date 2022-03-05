4b3618cc39bd7ead04831d7e1f823b644fd7ac4d
Caio Jannuzzi <eng.caiojannuzzi@outlook.com>
2022-03-05 14:49:31 -0300

    Criar arquivo objeto.js para praticar no primeiro contato fundamentos em Objetos

---
+++ objeto.js
@@ -0,0 +1,10 @@
+const listaCPFs = ["1235678954","8459761430","9873816719"];
+
+// const cliente = ["nome", "Andre", "idade", 36];
+
+const cliente = {
+    nome: "Andre",
+    idade: 36,
+    cpf: "1235678954",
+    email: "andre@gmail.com"
+}
